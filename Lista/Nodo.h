114f4c6870f1de2c9e9f177aa755d9ba4532821c
agustinreyna <reyna.agustin2003@gmail.com>
2023-10-18 06:28:36 -0300

    cambios

---
+++ Lista/Nodo.h
@@ -0,0 +1,39 @@
+#ifndef NODO_H
+#define NODO_H
+#include <iostream>
+
+using namespace std;
+
+class Nodo{
+    private:
+    string codigo,articulo;
+    int deposito1,deposito2,deposito3,deposito4,deposito5;
+    Nodo *sgte;
+    public:
+    string getCodigo(){
+        return codigo;
+    }
+    string getArticulo(){
+        return articulo;
+    }
+    int getDeposito1(){
+        return deposito1;
+    }
+    int getDeposito2(){
+        return deposito2;
+    }
+    int getDeposito3(){
+        return deposito3;
+    }
+    int getDeposito4(){
+        return deposito4;
+    }
+    int getDeposito5(){
+        return deposito5;
+    }
+    int getDepositoTotal(){
+        return deposito1+deposito2+deposito3+deposito4+deposito5;
+    }
+    
+};
+#endif NODO_H
