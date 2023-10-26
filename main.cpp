bfd9c0a1a1e271cb616884383dec12c2db94a83c
agustinreyna <reyna.agustin2003@gmail.com>
2023-10-25 22:54:10 -0300

    avancess

--- main.cpp
+++ main.cpp
@@ -2,65 +2,105 @@
 #include <map>
 #include <string>
 #include <vector>
+#include <fstream>
+#include <sstream>
 
 struct Articulo
 {
-    string codigo, grupo;
+    std::string codigo, grupo;
     int deposito1, deposito2, deposito3, deposito4, deposito5;
 };
 
 struct stockArticulo
 {
-    string codigo, nombre, grupo;
+    std::string codigo, nombre, grupo;
 };
+
 
 using namespace std;
 
 int main()
 {
     map<string, Articulo> inventario;
-    map<int[6],stockArticulo> inventarioStock;
-    string nombre;
-    int k, totalarticulos = 0, totalarticulosdiferentes = 0;
+    map<vector<int>,stockArticulo> inventarioStock;
+    string nombre,line,stringNum;
+    int k, totalarticulos = 0, totalarticulosdiferentes = 0,num;
     Articulo articulo;
     stockArticulo stocks;
     vector<int> depositos;
-    do
-    {
-        cout << "-----------------------------" << endl;
-        cout << "Decime el nombre" << endl;
-        cin >> nombre;
-        cout << "Decime el codigo" << endl;
-        cin >> articulo.codigo;
-        cout << "Decime el grupo" << endl;
-        cin >> articulo.grupo;
-        cout << "Decime la cantidad en deposito 1" << endl;
-        cin >> articulo.deposito1;
-        cout << "Decime la cantidad en deposito 2" << endl;
-        cin >> articulo.deposito2;
-        cout << "Decime la cantidad en deposito 3" << endl;
-        cin >> articulo.deposito3;
-        cout << "Decime la cantidad en deposito 4" << endl;
-        cin >> articulo.deposito4;
-        cout << "Decime la cantidad en deposito 5" << endl;
-        cin >> articulo.deposito5;
+
+    ifstream csv("csv.csv");
+
+    while (getline(csv, line)) {
+        stringstream ss(line);
+
+        // Usamos getline con ',' como delimitador para obtener cada campo
+        getline(ss, articulo.grupo, ',');
+        getline(ss, articulo.codigo, ',');
+        getline(ss, nombre, ',');
+        getline(ss, stringNum, ',');
+        if (!stringNum.empty()) {
+        num = stoi(stringNum);
+        }
+        else num = 0;
+        articulo.deposito1=num;
+        getline(ss, stringNum, ',');
+        if (!stringNum.empty()) {
+        num = stoi(stringNum);
+        }
+        else num = 0;
+        articulo.deposito2=num;
+        getline(ss, stringNum, ',');
+        if (!stringNum.empty()) {
+        num = stoi(stringNum);
+        }
+        else num = 0;
+        articulo.deposito3=num;
+        getline(ss, stringNum, ',');
+        if (!stringNum.empty()) {
+        num = stoi(stringNum);
+        }
+        else num = 0;
+        articulo.deposito4=num;
+        getline(ss, stringNum, '\n');
+        if (!stringNum.empty()) {
+        num = stoi(stringNum);
+        }
+        else num = 0;
+        articulo.deposito5=num;
+
+        stocks.grupo=articulo.grupo;
+        stocks.nombre=nombre;
+        stocks.codigo=articulo.codigo;
+        depositos.push_back(articulo.deposito1 + articulo.deposito2 + articulo.deposito3 + articulo.deposito4 + articulo.deposito5);
+        depositos.push_back(articulo.deposito1);
+        depositos.push_back(articulo.deposito2);
+        depositos.push_back(articulo.deposito3);
+        depositos.push_back(articulo.deposito4);
+        depositos.push_back(articulo.deposito5);
 
         totalarticulos += articulo.deposito1 + articulo.deposito2 + articulo.deposito3 + articulo.deposito4 + articulo.deposito5;
-
         totalarticulosdiferentes++;
-
+        cout<<totalarticulosdiferentes<<endl;
         inventario.insert(make_pair(nombre, articulo));
-        cout << "Si queres terminar de insertar articulos ingresa 0" << endl;
-        cin >> k;
-    } while (k != 0);
+        inventarioStock.insert(make_pair(depositos,stocks));
+    }
 
-    for (auto pair : inventario)
+    for (auto pair : inventarioStock)
     {
         cout << "-----------------------------" << endl;
-        cout << pair.first << endl;
+        cout << pair.second.nombre << endl;
     }
     cout << "-----------------------------" << endl;
     cout << "Total de articulos: " << totalarticulos << endl;
     cout << "Total de articulos diferentes: " << totalarticulosdiferentes << endl;
+
+
+
+
+    
     return 0;
 }
+
+
+
